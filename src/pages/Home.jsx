dfd8da8698b467e0fac72bf2f63a1f8f89fb8308
muskanqed <muskanqed@gmail.com>
2025-01-21 09:09:26 +0530

    Added link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 
 export default function HomePage() {
   return (
+
     <div>
+      <Link to={"/dashboard"}> muskan</Link>
       <div></div>Home Page
     </div>
   );
